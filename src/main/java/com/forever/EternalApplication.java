de7ef3a28456da2bae683e0cf4642605617005f4
MacSierra <summit1991@naver.com>
2018-07-30 22:05:42 +0900

    초기화

---
+++ src/main/java/com/forever/EternalApplication.java
@@ -0,0 +1,12 @@
+package com.forever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternalApplication.class, args);
+	}
+}
